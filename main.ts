758b45a3ed6563cedceb74425821d8ed49837016
abinnovator <89593957+abinnovator@users.noreply.github.com>
2021-09-13 17:30:20 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 let Reading = 0
 led.setBrightness(64)
+radio.setTransmitSerialNumber(true)
+radio.setGroup(4)
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P1, 1023)
     Reading = pins.analogReadPin(AnalogPin.P0)
+    radio.sendNumber(Reading / 4)
     pins.analogWritePin(AnalogPin.P0, 0)
     led.plotBarGraph(
     pins.analogReadPin(AnalogPin.P0),
